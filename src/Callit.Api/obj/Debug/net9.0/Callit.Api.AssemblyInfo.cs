ad5cfe8e7f4b02bcfb21e1856bd7a64cd7005e66
Luis Carlos Silva <luis.silva@smgbit.com.br>
2025-02-18 11:20:20 -0300

    Update binary files and project restore information

--- src/Callit.Api/obj/Debug/net9.0/Callit.Api.AssemblyInfo.cs
+++ src/Callit.Api/obj/Debug/net9.0/Callit.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Callit.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b47ee34a737d921af5e38ba22a787f29cfc3e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e263f4dea42e9167c3755b301416c4c1dd144ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Callit.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Callit.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ tests/Validators.Tests/Tickets/CreateTicket/CreateTicketValidatorTest.cs
@@ -0,0 +1,28 @@
+﻿using Callit.Application.UseCases.Tickets;
+using Callit.Communication.Enums;
+using Callit.Communication.Requests;
+
+namespace Validators.Tests.Tickets.CreateTicket;
+
+public class CreateTicketValidatorTest
+{
+	[Fact]
+	public void CreateTicketSucessfully()
+	{
+		var validator = new TicketValidator();
+
+		var request = new RequestTicketJson
+		{
+			Title = "Title",
+			Description = "Description",
+			Date = DateTime.Now,
+			Priority = Priority.Low,
+			Status = TicketStatus.Waiting,
+			System = "System",
+		};
+		
+		var ticket = validator.Validate(request);
+		
+		 Assert.True(ticket.IsValid);
+	}
+}
