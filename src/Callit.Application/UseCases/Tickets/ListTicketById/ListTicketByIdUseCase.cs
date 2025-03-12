11eb246a5a35f97899e8b908877188f6c68c20fa
Luis Carlos Silva <luis.silva@smgbit.com.br>
2025-03-12 20:26:31 -0300

    Update binary files and project assets for improved functionality

--- src/Callit.Application/UseCases/Tickets/ListTicketById/ListTicketByIdUseCase.cs
+++ src/Callit.Application/UseCases/Tickets/ListTicketById/ListTicketByIdUseCase.cs
@@ -9,32 +9,32 @@ namespace Callit.Application.UseCases.Tickets.ListTicketById;
 
 public class ListTicketByIdUseCase : IListTicketByIdUseCase
 {
-	private readonly ITicketRepository _ticketRepository;
-	private readonly IMapper _mapper;
-	private readonly ILoggedUser _loggedUser;
-	
-	public ListTicketByIdUseCase(
-		ITicketRepository ticketRepository,
-		IMapper mapper,
-		ILoggedUser loggedUser
-		)
-	{
-		_ticketRepository = ticketRepository;
-		_mapper = mapper;
-		_loggedUser = loggedUser;
-	}
-	
-	public async Task<ResponseTicketJson> Execute(Guid id)
-	{
-		var loggedUser = await _loggedUser.GetUser();
-		
-		var ticket = await _ticketRepository.GetTicketById(id, loggedUser);
+    private readonly ITicketRepository _ticketRepository;
+    private readonly IMapper _mapper;
+    private readonly ILoggedUser _loggedUser;
 
-		if (ticket is null || ticket.UserId != loggedUser.Id)
-		{
-			throw new NotFoundException("Ticket not found");
-		}
+    public ListTicketByIdUseCase(
+        ITicketRepository ticketRepository,
+        IMapper mapper,
+        ILoggedUser loggedUser
+    )
+    {
+        _ticketRepository = ticketRepository;
+        _mapper = mapper;
+        _loggedUser = loggedUser;
+    }
 
-		return _mapper.Map<ResponseTicketJson>(ticket);
-	}
+    public async Task<ResponseTicketJson> Execute(Guid id)
+    {
+        var loggedUser = await _loggedUser.GetUser();
+
+        var ticket = await _ticketRepository.GetTicketById(id, loggedUser);
+
+        if (ticket is null || ticket.UserId != loggedUser.Id)
+        {
+            throw new NotFoundException("Ticket not found");
+        }
+
+        return _mapper.Map<ResponseTicketJson>(ticket);
+    }
 }
--- src/Callit.Infrastructure/obj/Debug/net9.0/Callit.Infrastructure.AssemblyInfo.cs
+++ src/Callit.Infrastructure/obj/Debug/net9.0/Callit.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Callit.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad5cfe8e7f4b02bcfb21e1856bd7a64cd7005e66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287753a4020896b048b3311a802f3d5bd160de6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Callit.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Callit.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
